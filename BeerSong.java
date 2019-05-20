d21f7133db9194da4860fec9c699edce406bf4c9
harikrishnanganapathy <50821286+harikrishnanganapathy@users.noreply.github.com>
2019-05-20 18:33:31 +0530

    Add files via upload

---
+++ BeerSong.java
@@ -0,0 +1,20 @@
+public class BeerSong{
+public static void main(String[] args){
+int beerNum =99;
+String word="bottles";
+while (beerNum > 0){
+if(beerNum==1){
+word="bottle";
+}
+System.out.println(beerNum +""+word+ "of beer on the wall");
+System.out.println(beerNum +""+word+ "of beer");
+System.out.println("take one down.");
+System.out.println("pass it arround");
+beerNum=beerNum-1;
+if(beerNum <0){
+System.out.println("no more bottles of beer on the wall");
+}
+}
+}
+}
+
