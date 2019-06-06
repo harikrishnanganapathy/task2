a1d4ae1a2f2b04bdfad6645134ded22e6a635b2f
harikrishnanganapathy <50821286+harikrishnanganapathy@users.noreply.github.com>
2019-06-06 19:01:26 +0530

    Add files via upload

--- xo.java
+++ xo.java
@@ -1,14 +1,17 @@
 import java.util.*;
 class tictac{
-    int n;
+    int n=3;
     int value;
     int x;
     int y;
+int turn;
     public void display(ArrayList<Integer> pos)
     {
         int position=0;
-        for(x=0;x<n;x++){
-            for(y=0;y<n;y++){
+        for(x=0;x<n;x++)
+	{
+            for(y=0;y<n;y++)
+		{
                System.out.print(pos.get(position));
                 position=position+1;
             }
@@ -17,30 +20,59 @@ class tictac{
 }
     int b; 
     int c;
+int a=0;
     Scanner sc=new Scanner(System.in);
     public void getinput(ArrayList<Integer> pos)
     {
-        
-        //do{
-            //display(pos);
+do{
+        turn=checkifplayersturn();
             System.out.println("enter the position");
             b=sc.nextInt();
             b=b-1;
+		
             if(pos.get(b)!=0)
             {
                 System.out.println("place is filled");
+		a=a-1;
             }
             else
             { 
-                System.out.println("enter 2 for x and 4 for o");
-                c=sc.nextInt();
+             //   System.out.println("enter 2 for x and 4 for o");
+                if(turn==1)
+		{
+			c=2;
+			turn=turn+1;
+		}
+		if(turn==0)
+		{
+			c=4;
+			turn=turn+1;
+		}
                 Integer cwrap=c;
                     pos.set(b,cwrap);
                     check(pos);
             }
-        //}while(pos.contains(0));
+		display(pos);
+		check(pos);
+a=a+1;
+        }while(pos.contains(0));
         //System.out.println("draw! game ends");
     }
+	int checkifplayersturn(){
+		if(a%2==0)
+		{
+			System.out.println("x's turn");
+turn=1;
+
+		}
+		else
+		{
+			System.out.println("y's turn");
+		turn=0;
+		}
+return turn;
+	}
+	//}
     public void check(ArrayList<Integer> pos)
     {
         int po=0;
@@ -50,7 +82,6 @@ class tictac{
         {
             for(int j=0;j<n;j++)
             {
-                po=po+1;
                 if(i==j){
                     if(pos.get(po)==2)
                     {
@@ -60,17 +91,73 @@ class tictac{
                             System.out.println("x wins");
                         }
                     }
-                    else if(pos.get(po)==4)
-                    {
+                    else
+			{ if(pos.get(po)==4)
+				{
                         flag=flag+1;
                         if(flag==3)
                         {
-                            System.out.println("y wins");
-                        }
+                            System.out.println("o wins");
+                        }}
                     }
-                    }
+	}po=po+1;
             }
         }
+			int posi=0;
+			int cnt=0;
+			int ct=0;
+			for(int i=0;i<n;i++)
+        		{
+            			for(int j=0;j>n;j++)
+            			{
+					if(posi%2==0)
+					{
+						if(pos.get(posi)==2)
+						{
+						cnt=cnt+1;
+							if(cnt==3)
+							{
+							System.out.println("x won");
+							}
+						}
+						else{
+							if(pos.get(posi)==4)
+							{
+								ct=ct+1;
+								if(ct==3)
+									{
+										System.out.println("o wins");}
+								}
+						}
+					posi=posi+1;
+					}
+				}
+			}
+int posit=0;
+int countt=0;
+int cot=0;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++){
+			if(pos.get(posit)==2){
+				countt=countt+1;
+					if(countt==3)
+					{
+					System.out.println("x wins");
+					}
+				}
+				else
+				{if(pos.get(j)==4){
+					cot=cot+1;
+					if(cot==3){
+						System.out.println("y wins");
+					}
+				}
+				
+			}posit=posit+1;
+		}
+	}
+
     }
 }
 public class xo{
@@ -88,11 +175,6 @@ public class xo{
         int y;
         tictac p1=new tictac();
         //xo p2=new xo();
-    //    for(x=0;x<m;x++)
-      //  {
-        //    for(y=0;y<n;y++)
-          //  {
-          //int position=0;
          ArrayList<Integer>pos =new ArrayList<Integer>();
     pos.add(0);
     pos.add(0);
@@ -103,14 +185,9 @@ public class xo{
     pos.add(0);
     pos.add(0);
     pos.add(0);
-                //arr[x][y]=0;
-            //}
-        //}
-do{
      p1.display(pos);
     p1.getinput(pos);
      p1.check(pos);
         //p2.player();
-}while(pos.contains(0));
      }
 }
